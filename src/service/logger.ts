a6c373d79d44a5a891397d4f346e23fa80915240
Dhanush <dhanush@Manjula-mac.local>
2024-05-20 16:06:03 +0530

    initial commit

--- src/service/logger.ts
+++
@@ -1,6 +0,0 @@
-import { Effect, Context } from "effect";
-
-export class Logger extends Context.Tag("Logger")<
-  Logger,
-  { readonly log: (message: string) => Effect.Effect<void> }
->() {}
--- src/service/random.ts
+++
@@ -1,5 +0,0 @@
-import { Context, Effect } from "effect";
-
-export class Random extends Context.Tag("MyRandomService")<Random, {
-    readonly next: Effect.Effect<number>
-}>(){}
